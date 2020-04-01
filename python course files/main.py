4e863ebcebc3b16617dae69c1bb33a6bf57198e0
B-Grylls <1965318205@qq.com>
2020-04-01 20:16:05 +0800

    delete files

--- python course files/main.py
+++
@@ -1,7 +0,0 @@
-from utils import fib
-import sys
-
-if __name__ == '__main__':
-	print(sys.argv)
-	n = int(sys.argv[1])
-	print(fib(n=n))
--- python course files/utils.py
+++
@@ -1,7 +0,0 @@
-def fib(n=20):
-    fibs=[0,1]
-    if n<2:
-        return fibs[:n]
-    for i in range(n-2):
-        fibs.append(fibs[-1] + fibs[-2])
-    return fibs
